4edc565de8cbe12c5454f2da89a35ffca176d70e
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-11-06 15:47:06 -0300

    1.0

---
+++ FrontEnd.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class FrontEnd extends JPanel{
+
+    
+
+    @Override
+    public void setVisible(boolean aFlag) {
+        super.setVisible(aFlag);
+    }  
+}
---
+++ MainActivity.java
@@ -0,0 +1,9 @@
+// front e backend se conectam
+
+public class MainActivity{
+    public static void main(String[] args){
+        FrontEnd FE = new FrontEnd();
+
+        FE.setVisible(true);
+    }
+}
