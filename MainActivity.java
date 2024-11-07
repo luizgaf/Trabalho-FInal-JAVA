1f24be4edeb5472d7f06dc8c611ea11ced049a33
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-11-07 15:31:20 -0300

    delete

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,6 @@
 // front e backend se conectam
-import front.*;
-import javax.swing.*;
-
 public class MainActivity{
     public static void main(String[] args){
-        JFrame frame = new JFrame("Banco");
-        frame.setSize(800, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // painel
-        FrontEnd frontEndPanel = new FrontEnd();
-        frame.add(frontEndPanel);
-
-        // Exibindo a janela
-        frame.setVisible(true);
+        
     }
 }
