6e339f246ef74f7a80ce2238468a8a6e11ced3cc
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-11-13 11:01:39 -0300

    update

--- Defesa_Civil/src/main/java/com/view/Defesa_Civil/FrontEndCadastroMembro.java
+++ Defesa_Civil/src/main/java/com/view/Defesa_Civil/FrontEndCadastroMembro.java
@@ -1,12 +1,19 @@
 package com.view.Defesa_Civil;
 
 import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
 import javax.swing.*;
-
+import javax.imageio.*;
+import java.io.*;
 
 public class FrontEndCadastroMembro extends JPanel{
+    private BufferedImage image;
+    private JPanel imagePanel;
+    private JFrame frame;
     
     public FrontEndCadastroMembro(JFrame frame){
+        this.frame = frame;
         setLayout(new BorderLayout());
 
         JLabel label = new JLabel("Cadastro Membro", JLabel.CENTER);
@@ -14,7 +21,8 @@ public class FrontEndCadastroMembro extends JPanel{
         label.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         add(label, BorderLayout.NORTH);
 
-        JPanel inputs = new JPanel(new FlowLayout());
+        JPanel inputs = new JPanel();
+        inputs.setLayout(new BoxLayout(inputs, BoxLayout.Y_AXIS));
         
         JPanel nomePanel = new JPanel(new FlowLayout());
         JLabel name = new JLabel("Nome Completo:");
@@ -47,13 +55,96 @@ public class FrontEndCadastroMembro extends JPanel{
         emailField.setPreferredSize(new Dimension(200, 30));
         dataPanel2.add(email); dataPanel2.add(emailField);
 
+        JPanel dataPanel3 = new JPanel(new FlowLayout());
         JLabel telEmergencia = new JLabel("Telefone Emergencia:");
         JTextField telEmergenciaField = new JTextField();
         telEmergenciaField.setPreferredSize(new Dimension(150, 30));
-        dataPanel2.add(telEmergencia); dataPanel2.add(telEmergenciaField);
+        dataPanel3.add(telEmergencia); dataPanel3.add(telEmergenciaField);
 
-        inputs.add(nomePanel); inputs.add(dataPanel); inputs.add(dataPanel2);
-        add(inputs, BorderLayout.CENTER);
+        JLabel uploadFoto = new JLabel("Upload Foto:");
+        dataPanel3.add(uploadFoto);
+        JButton uploaderFoto = new JButton("Selecionar Imagem");
+        uploaderFoto.addActionListener(e -> selectImage());
+        dataPanel3.add(uploaderFoto);
         
+        JPanel dataPanel4 = new JPanel(new FlowLayout());
+        imagePanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (image != null) {
+                    g.drawImage(image, 0, 0, this.getWidth(), this.getHeight(), this);
+                }
+            }
+        };
+        imagePanel.setPreferredSize(new Dimension(200, 200));
+        dataPanel4.add(imagePanel);
+
+
+        inputs.add(nomePanel); 
+        inputs.add(dataPanel); 
+        inputs.add(dataPanel2); 
+        inputs.add(dataPanel3);
+        inputs.add(dataPanel4);
+
+        add(inputs, BorderLayout.CENTER);
+
+        JButton submitButton = new JButton("Cadastrar");
+        JButton cancelButton = new JButton("Cancelar");
+        addHoverEffect(submitButton);
+        addHoverEffect(cancelButton);
+
+        submitButton.addActionListener(e -> adcMembro());
+        cancelButton.addActionListener(e -> voltar());
+
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(submitButton);
+        buttonPanel.add(cancelButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+    public void addHoverEffect(JButton button) {
+        button.setBackground(new Color(50,50,50));
+        button.setForeground(Color.white);
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        button.setPreferredSize(new Dimension(200, 50));
+
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                button.setBackground(Color.BLACK); 
+                button.setForeground(Color.WHITE); 
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                button.setBackground(new Color(50, 50, 50)); 
+                button.setForeground(Color.WHITE); 
+            }
+        });
+    }
+
+    private void selectImage() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Imagens", "jpg", "png", "jpeg"));
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            try {
+                image = ImageIO.read(selectedFile);
+                imagePanel.repaint(); // Atualiza o painel para exibir a imagem
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(this, "Erro ao carregar a imagem", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void adcMembro(){};
+
+    private void voltar() {
+        if (frame != null) {
+            frame.dispose();
+        }
     }
 }
