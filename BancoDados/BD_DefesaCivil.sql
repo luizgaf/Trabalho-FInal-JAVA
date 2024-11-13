b047817e07069e24d27a9c9ee0e4bf7b62cfd98f
Lara Ewellen <laraewellen@gmail.com>
2024-11-13 02:31:38 -0300

    alteração no insert e no int

--- BancoDados/BD_DefesaCivil.sql
+++ BancoDados/BD_DefesaCivil.sql
@@ -1,40 +1,27 @@
 CREATE DATABASE IF NOT EXISTS DefesaCivil;
-
 USE DefesaCivil;
 
 CREATE TABLE IF NOT EXISTS Endereco(
-	idEndereco INT AUTO_INCREMENT,
+    idEndereco INT AUTO_INCREMENT,
     logradouro VARCHAR(100) NOT NULL,
     numero INT NOT NULL,
-    cidade VARCHAR (50) NOT NULL,
-    CEP INT(8) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    CEP CHAR(8) NOT NULL, 
     Complemento VARCHAR(100),
     PRIMARY KEY (idEndereco)
 );
 
 CREATE TABLE IF NOT EXISTS TipoRisco(
-	idRisco INT AUTO_INCREMENT,
+    idRisco INT AUTO_INCREMENT,
     categoria VARCHAR(100) NOT NULL,
     PRIMARY KEY (idRisco)
 );
 
-INSERT INTO TipoRisco (categoria) VALUES
-('Em risco'),
-('Alto Risco'),
-('Extremo Risco');
-
 CREATE TABLE IF NOT EXISTS TipoEmergencia(
-	idEmergencia INT AUTO_INCREMENT,
+    idEmergencia INT AUTO_INCREMENT,
     categoria VARCHAR(100) NOT NULL,
     PRIMARY KEY (idEmergencia)
 );
-INSERT INTO TipoEmergencia (categoria) VALUES
-('Inundação'),
-('Vendaval'),
-('Desmoronamentos'),
-('Incendio'),
-('Tsuname'),
-('Tempestade');
 
 CREATE TABLE IF NOT EXISTS CadastroFamilia(
     idFamilia INT AUTO_INCREMENT,
@@ -49,8 +36,7 @@ CREATE TABLE IF NOT EXISTS CadastroFamilia(
 CREATE TABLE IF NOT EXISTS Membro(
     CPF CHAR(11), 
     nome VARCHAR(100) NOT NULL,
-    numTelefone VARCHAR(15) NOT NULL, 
-    dataNasc DATE NOT NULL,
+    numTelefone VARCHAR(15) NOT NULL,  
     email VARCHAR(100) NOT NULL,
     telEmergencia VARCHAR(15) NOT NULL,
     Endereco_id INT,  
@@ -61,3 +47,15 @@ CREATE TABLE IF NOT EXISTS Membro(
 );
 
 
+INSERT INTO TipoRisco (categoria) VALUES
+('Em risco'),
+('Alto Risco'),
+('Extremo Risco');
+
+INSERT INTO TipoEmergencia (categoria) VALUES
+('Inundação'),
+('Vendaval'),
+('Desmoronamentos'),
+('Incendio'),
+('Tsuname'),
+('Tempestade');
