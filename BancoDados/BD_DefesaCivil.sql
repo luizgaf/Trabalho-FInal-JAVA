449b5d633db8349badee311d8793f3113453c77d
Julia-Gabriela <juliagabrielagomes09@gmail.com>
2024-11-18 22:36:35 -0300

    update

--- BancoDados/BD_DefesaCivil.sql
+++ BancoDados/BD_DefesaCivil.sql
@@ -1,28 +1,31 @@
 CREATE DATABASE IF NOT EXISTS DefesaCivil;
 USE DefesaCivil;
 
+CREATE TABLE Users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    UNIQUE(username)
+);
+
 CREATE TABLE IF NOT EXISTS Endereco(
-    idEndereco INT AUTO_INCREMENT,
     idEndereco INT AUTO_INCREMENT,
     logradouro VARCHAR(100) NOT NULL,
     numero INT NOT NULL,
-    cidade VARCHAR(50) NOT NULL,
     CEP CHAR(8) NOT NULL, 
     cidade VARCHAR(50) NOT NULL,
-    CEP CHAR(8) NOT NULL, 
     Complemento VARCHAR(100),
     PRIMARY KEY (idEndereco)
 );
 
 CREATE TABLE IF NOT EXISTS TipoRisco(
     idRisco INT AUTO_INCREMENT,
-    idRisco INT AUTO_INCREMENT,
     categoria VARCHAR(100) NOT NULL,
     PRIMARY KEY (idRisco)
 );
 
 CREATE TABLE IF NOT EXISTS TipoEmergencia(
-    idEmergencia INT AUTO_INCREMENT,
     idEmergencia INT AUTO_INCREMENT,
     categoria VARCHAR(100) NOT NULL,
     PRIMARY KEY (idEmergencia)
@@ -41,12 +44,13 @@ CREATE TABLE IF NOT EXISTS CadastroFamilia(
 CREATE TABLE IF NOT EXISTS Membro(
     CPF CHAR(11), 
     nome VARCHAR(100) NOT NULL,
-    numTelefone VARCHAR(15) NOT NULL,  
-    numTelefone VARCHAR(15) NOT NULL,  
+    numTelefone VARCHAR(15) NOT NULL, 
+    dataNasc date,
     email VARCHAR(100) NOT NULL,
     telEmergencia VARCHAR(15) NOT NULL,
     Endereco_id INT,  
     CadastroFamilia_id INT, 
+	foto LONGBLOB,
     PRIMARY KEY (CPF),
     FOREIGN KEY (Endereco_id) REFERENCES Endereco (idEndereco),
     FOREIGN KEY (CadastroFamilia_id) REFERENCES CadastroFamilia (idFamilia)
@@ -65,6 +69,9 @@ INSERT INTO TipoEmergencia (categoria) VALUES
 ('Incendio'),
 ('Tsuname'),
 ('Tempestade');
+
+INSERT INTO Users (username, password, role) VALUES ('admin', '12345', 'ADMIN');
+
 -- Desabilitar modo de segurança da query (permite usar update e delete nos dados do banco)
 SET SQL_SAFE_UPDATES = 0;
 
@@ -76,4 +83,7 @@ update endereco set Complemento = 'Apartamento' where idEndereco in (4);
 
 SELECT*
 FROM endereco;
-
+SELECT*
+FROM users;
+SELECT*
+FROM membro;
